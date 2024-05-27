6af8be0adfeb2a75d90cd0ff02d9edb3a0bfc2c5
Thomas Mascheretti <thomasmascheretti@yahoo.fr>
2024-05-27 16:05:08 +0200

    [TAS-32] add status column to booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_120316) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_121555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_bookings_on_offer_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.float "rating"
+    t.bigint "offer_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_comments_on_offer_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.integer "year"
+    t.integer "km"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +63,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_120316) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "offers"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "comments", "offers"
+  add_foreign_key "comments", "users"
+  add_foreign_key "offers", "users"
 end
