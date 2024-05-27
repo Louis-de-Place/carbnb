7481b2d357e01a5ac5b36e9417acdd47b29b7c52
Manon Reynaud <manon.reynaud2@gmail.com>
2024-05-27 16:23:29 +0200

    [TAS-16] created routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :offers, except: [:edit] do
+    resources :bookings, only: [:new, :create,]
+    resources :reviews, only: [:create]
+  end
+  resources :bookings, only: [:destroy]
+  resources :reviews, only: [:destroy]
 end
