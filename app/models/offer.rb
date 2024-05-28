4909926010db7d5c9dbf3ec942fe93bb02db2d32
Raphael Ainardi <ainardi.raphael@icloud.com>
2024-05-28 11:32:34 +0200

    modif model offer and model user

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :bookings
-  has_many :offers, dependent: :destroy
   validates :brand, :model, :year, :km, presence: true
 end
