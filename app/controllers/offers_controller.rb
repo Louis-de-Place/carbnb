d4ef949d120188aa4384587dfbd63c0187c7801d
Raphael Ainardi <ainardi.raphael@icloud.com>
2024-05-28 12:43:13 +0200

    [TAS-35] Update offers controller methods/modif index view

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,19 +1,40 @@
 class OffersController < ApplicationController
   def index
+    @offers = Offer.all
   end
 
   def show
+    @offer = Offer.find(params[:id])
   end
 
   def new
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(params[:offer])
+    if @offer.save
+      redirect_to offer_path(@offer), notice: 'Offer was successfully created.'
+    else
+      render :new, alert: 'There was an error creating the offer.'
+    end
   end
 
   def update
+    @offer = Offer.find(params[:id])
+    @offer.update(offer_params)
+    redirect_to offer_path(@offer)
   end
 
   def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to offer_path, status: :see_other
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:brand, :model, :year, :km, :price, :title)
   end
 end
