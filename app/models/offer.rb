0248b6f7ee4428e4af97c15102ba51b7e6bda90a
Raphael Ainardi <ainardi.raphael@icloud.com>
2024-05-29 11:54:07 +0200

    [TAS-38] update seed with description

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,7 +2,7 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :bookings
-  validates :title, :price, :brand, :model, :year, :km, presence: true
+  validates :price, :brand, :model, :year, :km, :address, :availability_end, :availability_start, presence: true
 
   has_many_attached :photos
 end
