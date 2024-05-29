afb902f4bba08548896d5a9d24af9aaf3efb681e
Thomas Mascheretti <thomasmascheretti@yahoo.fr>
2024-05-29 14:03:35 +0200

    [TAS-29]setup mapbox

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,7 @@ class Offer < ApplicationRecord
   validates :price, :brand, :model, :year, :km, :address, :availability_end, :availability_start, presence: true
 
   has_many_attached :photos
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
