3df4dd6728402e58d35a68ea74235f1bea1c1366
Thomas Mascheretti <thomasmascheretti@yahoo.fr>
2024-05-29 11:17:00 +0200

    [TAS-19] update offers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_104043) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_091411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,7 +63,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_104043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
-    t.string "title"
+    t.string "address"
+    t.date "availability_start"
+    t.date "availability_end"
+    t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
