3235bbadea5a06655de8b07fd5b8ad1ddc20fbd6
Louis de Place <louis@de-place.fr>
2024-05-29 14:35:51 +0200

    Important push before prez

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "/success", to: "pages#success", as: :success
 
   resources :offers, except: [:edit] do
     resources :bookings, only: [:new, :create,]
