8c567f386f9bc1c019f6b06a61e67b36dbe707f5
Thomas Mascheretti <thomasmascheretti@yahoo.fr>
2024-05-30 15:36:40 +0200

    no list display with map

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,15 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["togglableElement", "buttonText"];
+  static targets = ["togglableElement", "buttonText", "cards"];
 
   connect() {
     console.log("Hello from toggle_controller.js");
+    console.log(this.cardsTarget)
   }
 
   fire() {
     this.togglableElementTarget.classList.toggle("disapear");
     this.updateButtonText();
+    this.cardsTarget.classList.toggle("disapear");
   }
 
   updateButtonText() {
